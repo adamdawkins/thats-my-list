76fe6acdf9695a6b9ad05a09e7a8cc49f84bc026
Glitch (economic-ptarmigan) <none>
2018-07-06 11:52:15 +0000

    🐯🐥 Updated with Glitch

--- server.js
+++ server.js
@@ -1,30 +1,20 @@
-// server.js
-// where your node app starts
-
-var compression = require('compression');
-var cors = require('cors');
 var express = require('express');
-var nocache = require('node-nocache');
+var cors = require('cors');
 
 var app = express();
 
-// compress our client side content before sending it over the wire
-app.use(compression());
-
 // your manifest must have appropriate CORS headers, you could also use '*'
-app.use(cors({ origin: 'https://trello.com' }));
-
-// https://github.com/mingchen/node-nocache
-app.use('/manifest.json', nocache, function (request, response) {
-  response.sendFile(__dirname + '/public/manifest.json');
-});
+app.use(cors({ origin: '*' }));
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
-  console.info(`Node Version: ${process.version}`);
-  console.log('Trello Power-Up Server listening on port ' + listener.address().port);
+// http://expressjs.com/en/starter/basic-routing.html
+app.get("*", function (request, response) {
+  response.sendFile(__dirname + '/views/index.html');
 });
 
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
+  console.log('Your app is listening on port ' + listener.address().port);
+});
