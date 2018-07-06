ef98a2db8b3e1610b9d085e5c213c81b17e7c321
Glitch (economic-ptarmigan) <none>
2018-07-06 12:09:33 +0000

    👦😿 Updated with Glitch

--- public/js/client.js
+++ public/js/client.js
@@ -4,18 +4,31 @@ var Promise = TrelloPowerUp.Promise;
 
 var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
 
-TrelloPowerUp.initialize({
-  // Start adding handlers for your capabilities here!
-	// 'card-buttons': function(t, options) {
-	// 	return [{
-	// 		icon: BLACK_ROCKET_ICON,
-	// 		text: 'Estimate Size',
-	// callback: function(t) {
-	// return t.popup({
-	// title: "Estimation",
-	// url: 'estimate.html',
-	// });
-	// }
-	// 	}];
-	// },
+window.TrelloPowerUp.initialize({
+	'list-actions': function (t) {
+		return t.list('name', 'id')
+			.then(function (list) {
+        console.log('list', {list})
+				return [{
+					text: "Assign to...",
+					callback: function (t) {
+						// Trello will call this if the user clicks on this action
+						// we could for example open a new popover...
+						t.popup({
+							title: 'Assign users',
+							items: [{
+								text: 'User 1',
+								callback: function (t, opts) { alert('User 1') }
+							}, {
+								text: 'User 2',
+								callback: function (t, opts) { alert('User 2') }
+							}, {
+								text: 'User 3',
+								callback: function (t, opts) { alert('User 3') }
+							}]
+						});
+					}
+				}];
+			});
+	}
 });
