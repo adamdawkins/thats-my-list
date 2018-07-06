9de51324c04f35053fcda4107b3d4f196cdc31a3
Adam Dawkins <adam@dragondrop.uk>
2018-07-06 12:56:19 +0100

    Try to add a list menu

--- public/js/client.js
+++ public/js/client.js
@@ -5,17 +5,29 @@ var Promise = TrelloPowerUp.Promise;
 var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
 
 TrelloPowerUp.initialize({
-  // Start adding handlers for your capabilities here!
-	// 'card-buttons': function(t, options) {
-	// 	return [{
-	// 		icon: BLACK_ROCKET_ICON,
-	// 		text: 'Estimate Size',
-	// callback: function(t) {
-	// return t.popup({
-	// title: "Estimation",
-	// url: 'estimate.html',
-	// });
-	// }
-	// 	}];
-	// },
+	'list-actions': function (t) {
+		return t.list('name', 'id')
+			.then(function (list) {
+				return [{
+					text: "Get List Stats",
+					callback: function (t) {
+						// Trello will call this if the user clicks on this action
+						// we could for example open a new popover...
+						return t.popup({
+							title: 'Snooze Card',
+							items: [{
+								text: 'User 1',
+								callback: function (t, opts) { alert('User 1') }
+							}, {
+								text: 'User 2',
+								callback: function (t, opts) { alert('User 2') }
+							}, {
+								text: 'User 3',
+								callback: function (t, opts) { alert('User 3') }
+							}]
+						});
+					}
+				}];
+			});
+	}
 });
