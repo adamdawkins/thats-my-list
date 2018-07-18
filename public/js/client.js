9a230db8e97ff0e3647734f905b77a469e421c71
Glitch (thats-my-list) <none>
2018-07-18 19:08:13 +0000

    🍬📹 Updated with Glitch

--- public/js/client.js
+++ public/js/client.js
@@ -1,72 +1,70 @@
 /* global TrelloPowerUp, Trello */
 
 var Promise = TrelloPowerUp.Promise;
-const token = localStorage.getItem('trello_token')
+	const token = localStorage.getItem('trello_token')
 Trello.setToken(token)
+	var POWER_UP_ROOT = 'https://thats-my-list.glitch.me'
+	var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
 
-var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421';
-
-const assignAllCards = (t, opts, memberId) => {
-   
- 
-   
-   if(!token) {
-   return t.popup({
-     title: 'You need to authorize',
-     url: './auth.html'
-   })
-   }
-  
-  console.log({ token, authorized: Trello.authorized() })
-  
-  console.log({memberId, opts})
-  console.log({canWrite: t.memberCanWriteToModel('card')})
-  return t.list('cards')
-    .then((list) => {
-      list.cards.forEach(({id}) => (
-        Trello.post(
-          `cards/${id}/idMembers`,
-          { value: memberId },
-          (result) => (console.log('it worked!', result) ), 
-          (error) => (console.error('the error', error))
-        )
-      ))
-  })
+	const toggleMemberOnList = (t, listId, memberId) => {
+	t.get('board', 'shared', listId, null)
+		.then((listData) => {
+				const apiURL = `${POWER_UP_ROOT}/lists/${listId}/members/${memberId}`
+				if (listData && listData.memberIds.indexOf(memberId) > -1) {
+					$.ajax({
+						url: apiURL,
+						type: 'DELETE',
+						success: () => {
+						const memberIds = listData ? listData.memberIds : []
+						return t.set('board', 'shared', listId, { memberIds: memberIds.filter(id => id !== memberId) })
+						}
+					})
+				} else {
+					// add member
+					$.post(`${POWER_UP_ROOT}/lists/${listId}/members/${memberId}`, () => {
+						console.log('callback')
+						const memberIds = listData ? listData.memberIds : []
+						memberIds.push(memberId)
+						return t.set('board', 'shared', listId, { memberIds })
+					})
+				}
+		}) 
 }
 
+
 window.TrelloPowerUp.initialize({
-  'authorization-status': (t, options) => {
-   console.log(Trello.authorized())
-    return {
-      authorized: Trello.authorized()
-    }
-  },
-  'show-authorization': (t, options) => {
-    return t.popup({
-      title: "Get started with That's My List",
-      url: './auth.html',
-      height: 140,
-    })
-  },
+	'authorization-status': (t, options) => {
+			console.log(Trello.authorized())
+			return {
+				authorized: Trello.authorized()
+			}
+	},
+	'show-authorization': (t, options) => {
+		return t.popup({
+			title: "Get started with That's My List",
+			url: './auth.html',
+			height: 140,
+		})
+	},
 	'list-actions': function (t) {
+		const { list } = t.getContext()
 		return t.board('members', 'id')
 			.then(function (board) {
-        console.log({board})
-				return [{
-					text: "Assign to...",
-					callback: function (t) {
-					  t.popup({
-							title: 'Assign these cards to...',
-							items: board.members.map(({ id, fullName}) => ({
-							  text: fullName,
-							  callback: function(t, opts) {
-                  assignAllCards(t, opts, id);
-                  return t.closePopup()
-                }
-							}))
-            })
-				  }
-        }];
+				console.log(board.id) 
+				return t.get('board', 'shared', list)
+					.then((listData) => {
+						console.log({listData})
+						return [{
+							text: "Assign to...",
+							callback: function (t) {
+                 t.popup({
+                    title: "Get started with That's My List",
+                    url: './members.html',
+                    height: 380,
+                  })
+							}
+						}];
+					})
 			});
 	}
 })
--- server.js
+++ server.js
@@ -1,8 +1,61 @@
 var express = require('express');
 var cors = require('cors');
 var bodyParser = require('body-parser')
-
+var request = require('request')
 var app = express();
+
+var jonathan = '55c9e22d65f6abc235d2fee5'
+var theList = '5b3f56bbd7b627a11f616bd3'
+const db = require('./database');
+
+
+//db.remove({}, {multi: true})
+
+// Keep Glitch from sleeping by periodically sending ourselves a http request
+setInterval(function() {
+  console.log('❤️ Keep Alive Heartbeat');
+  request.get('https://thats-my-list.glitch.me')
+  .then(() => {
+    console.log('💗 Successfully sent http request to Glitch to stay awake.');
+  })
+  .catch((err) => {
+    console.error(`💔 Error sending http request to Glitch to stay awake: ${err.message}`);
+  });
+}, 150000); // every 2.5 minutes
+
+function assignCard({id, idList}) {
+  db.find({ listId: idList }, (err, docs) => {
+    if (err) {
+        console.error(err);
+        console.error(`Error trying to find list with id ${idList}`, err.message);
+    }
+    
+    if (docs.length === 0) {
+     return console.log(`We're not watching the list with id ${idList}, doing nothing`)
+    }
+    const { memberIds } = docs[0]
+    console.log(`Assigning the card (${id}) to members: ${memberIds}`)
+        request({
+    method: 'PUT',
+        uri: `https://api.trello.com/1/cards/${id}`,
+        qs: {
+          idMembers: memberIds.join(','),
+          key: process.env.APP_KEY,
+          token: process.env.TOKEN
+        },
+        json: true,
+        timeout: 120000
+    }, (error, res, billoddy) => {
+      console.log({billoddy})
+    })
+  })
+//   if (idList !== theList) {
+   
+//   }
+  
+//   console.log(`We need to assign the card to ${jonathan}`)
+
+}
 
 // your manifest must have appropriate CORS headers, you could also use '*'
 app.use(cors({ origin: '*' }));
@@ -10,9 +63,53 @@ app.use(cors({ origin: '*' }));
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 app.use(bodyParser.json())
+
+app.post('/lists/:listId/members/:memberId', (req, res, next) => {
+  const {listId, memberId }  = req.params
+  
+  db.update({ listId }, {$addToSet: { memberIds: memberId } }, { upsert: true }, function (err, added) {
+          if (err) {
+            console.error(err);
+            console.error(err.message);
+            return res.sendStatus(500);
+          }
+          console.log(`🎉 Added member (${memberId}) to list (${listId})`);
+          
+          return res.sendStatus(200);
+        });
+})
+
+app.delete('/lists/:listId/members/:memberId', (req, res, next) => {
+  const {listId, memberId}  = req.params
+
+  
+  db.update({ listId }, {$pull: { memberIds:  memberId } }, {}, function (err, added) {
+          if (err) {
+            console.error(err);
+            console.error(err.message);
+            return res.sendStatus(500);
+          }
+          console.log(`🎉 Removed member (${memberId}) from list (${listId})`);
+          return res.sendStatus(200);
+        });
+})
+
+app.get('/lists', (req, res, next) => {
+    db.find({}, function(err, docs) {
+    if (err) return console.error(err.message)
+    
+      res.send(docs)
+    })
+    
+})
+
 app.post('/board-webhook', (req, res, next) => {
   console.log('Webhook triggered!')
-  console.log(req.body)
+  if(req.body.action.display.translationKey === 'action_move_card_from_list_to_list') {
+    assignCard(req.body.action.data.card)
+  } else {
+    console.log('They did something else', req.body.action.display.translationKey)
+  }
   res.status(200).end()
 })
 
